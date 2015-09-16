1645b1481d01a7bf656cc2f7eedae6e9365735e9
BokilaLin <eet02948@gmail.com>
2015-09-16 17:27:22 +0800

    Add Adding/Editing/removing fumction

--- blog/views.py
+++ blog/views.py
@@ -41,3 +41,20 @@ def post_edit(request, post_id):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {"form":form})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by("created_date")
+    return render(request, "blog/post_draft_list.html", {"posts":posts})
+
+
+def post_publish(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.publish()
+    return redirect('blog.views.post_detail', post_id=post_id)
+
+
+def post_remove(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    post.delete()
+    return redirect('blog.views.post_list')
